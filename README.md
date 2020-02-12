eb12d317b27a5d187df362586d74c012c9ac0d7f
AerYue <2930789752@qq.com>
2020-02-12 18:26:24 +0800

    Add login to get user information

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 ##爱尔月 社区
 
 ##资料
-https://spring.io/guides
-https://spring.io/guides/gs/serving-web-content/
-https://developer.github.com/apps/building-oauth-apps/
+[Spring 文档](https://spring.io/guides)
+[Spring 开发步骤](https://spring.io/guides/gs/serving-web-content/)
+[Github Oauth 开发文档](https://developer.github.com/apps/building-oauth-apps/)
+[BootStrap](https://v3.bootcss.com/)
+
+
 ##工具
-git.exe
+[git工具](https://git-scm.com/downloads)
---
+++ src/main/java/com/aeryue/community/dto/GithubUser.java
@@ -0,0 +1,31 @@
+package com.aeryue.community.dto;
+
+public class GithubUser {
+    private String name;
+    private Long id;
+    private String dio;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setDio(String dio) {
+        this.dio = dio;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDio() {
+        return dio;
+    }
+}
